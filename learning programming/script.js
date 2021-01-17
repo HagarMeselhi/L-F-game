1691799e43419fa7e8cedeb9a805828954c69772
Aya Elsharawi <34321395+AyaElsharawy@users.noreply.github.com>
2021-01-17 19:59:36 +0200

    Style file for the list and JavaScript code

---
+++ learning programming/script.js
@@ -0,0 +1,40 @@
+var number1=document.getElementById("one");
+number1.addEventListener('click',function (e)
+{
+   location.replace('vedio1.html');
+});
+var number2=document.getElementById("two");
+number2.addEventListener('click',function (e)
+{
+   location.replace('video2.html');
+});
+var number3=document.getElementById("three");
+number3.addEventListener('click',function (e)
+{
+   location.replace('video3.html');
+});
+var number4=document.getElementById("four");
+number4.addEventListener('click',function (e)
+{
+   location.replace('video4.html');
+});
+var number5=document.getElementById("five");
+number5.addEventListener('click',function (e)
+{
+   location.replace('video5.html');
+});
+var number6=document.getElementById("six");
+number6.addEventListener('click',function (e)
+{
+   location.replace('video6.html');
+});
+var number7=document.getElementById("seven");
+number7.addEventListener('click',function (e)
+{
+   location.replace('video7.html');
+});
+var number8=document.getElementById("back");
+number8.addEventListener('click',function (e)
+{
+   location.replace('index.html');
+});
